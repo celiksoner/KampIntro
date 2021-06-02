ccc98d96b511a6fe8670eb40cb5f07aef613f1ea
Soner Çelik <snrclk94@outlook.com>
2021-06-02 18:13:14 +0300

    .

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //Encapsulation
+        public void Add(Product product)
+        {
+            product.ProductName = "Kamera";
+        }
+
+        public void BiseyYap (int sayi)
+        {
+            sayi = 99;
+        }
+
+
+    }
+}
--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,7 +6,28 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 200;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, ProductName = "Kalem" };
+
+            ProductManager productManager = new ProductManager();
+            
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+            int sayi = 100;
+            productManager.BiseyYap(sayi);
+            Console.WriteLine(sayi);
+
+
         }
+
+
+
     }
 }
