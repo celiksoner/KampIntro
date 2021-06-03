83b6fec753bf1bc5f309c01a0eb12b2498c80fd6
Soner Çelik <snrclk94@outlook.com>
2021-06-04 02:47:10 +0300

    04.06.2021 02:47

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -8,28 +8,45 @@ namespace GenericsIntro
 {
     public class MyList<T>
     {
+        //Tüm metodlardan ulaşılabilir bir array oluşturuyoruz.
         T[] items;
         
-
+        //Class her çağırıldığında costructor metoduyla yeni bir array oluşturuyoruz.
         public MyList()
         {
             items = new T[0];
         }
 
+
+        //Burada ekleme metodu çağırıldığında referans adresi değişeceği için önce mevcut dizimizin elemanlarını geçici bir diziye aktarıyoruz daha sonra
+        //Mevcut dizinin eleman sayısını bir arttırarak geçici diziden verileri geri alıyoruz. Bu işlemden sonra yeni gelen veriyi dizimizin son index'ine ekliyoruz.
+        //
         public void Add(T item)
         {
+            //Elimizdeki verileri geçici diziye aktarma komutu
             T[] tempArray = items;
+            //Mevcut dizimizin boyutunu 1 arttırıyoruz.
             items = new T[items.Length + 1];
 
+            //Döngü yardımı ile tempArray dizisinin boyutu kadar döngü ile verilerimizi mevcut dizimize çekiyoruz.
             for (int i = 0; i < tempArray.Length; i++)
             {
                 items[i] = tempArray[i];
             }
-
+            //Burada son olarak add metodundan gelen veriyi dizimizin son index'ine ekliyoruz.
             items[items.Length - 1] = item;
             
         }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
         
+        public T[] Items
+        {
+            get { return items; }
+        }
 
        
     }
--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -10,7 +10,16 @@ namespace GenericsIntro
 
             isimler.Add("Soner");
 
-            
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Caner");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
 
         }
     }
