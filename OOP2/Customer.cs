0b6198d1696e5a742e9667d91d6d316050e6e3d9
Soner Çelik <snrclk94@outlook.com>
2021-06-04 18:38:40 +0300

    .

---
+++ OOP2/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class Customer
+    {
+        //Eper bir nesne o sınıfa ait değil, gereksiz gibi duruyorsa kesinlikle bir yanlışlık vardır.
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string SirketAdi { get; set; }
+        public string TcNo { get; set; }
+        public string VergiNo { get; set; }
+
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Soner Çelik
+            Customer customer1 = new Customer();
+            customer1.Adi = "Soner";
+            customer1.Soyadi = "Celik";
+            customer1.Id = 1;
+            customer1.TcNo = "123466788";
+            customer1.MusteriNo = "1234";
+            customer1.SirketAdi = "Deneme";
+
+            // Gerçek Müşteri - Tüzel Müşteri
+            // SOLID
+
+
+        }
+    }
+}
