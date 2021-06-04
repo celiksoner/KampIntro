a541e041a24d7c1c507de30784eeb54d55b9acc7
Soner Çelik <snrclk94@outlook.com>
2021-06-04 18:00:56 +0300

    04.06.2021 18:00

---
+++ DictionaryYapısı/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryYapısı
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key,V value)
+        {
+            K[] tempKeys = keys; 
+            V[] tempValues = values; 
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length-1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public K[] Keys
+        {
+            get { return keys; }
+        }
+
+        public V[] Values
+        {
+            get { return values; }
+        }
+
+    }
+}
---
+++ DictionaryYapısı/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryYapısı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///*
+            // 1- Dictionary sınıfı içinde sizin belirleyeceğiniz bir key'e uygun olarak
+            //değer saklama zorunluluğu vardır.
+            //2- Key türünü belirtmek zorunludur.
+            //*/
+
+            //Dictionary<string, int> AdYas = new Dictionary<string, int>();
+            ////Veri ekleme
+            //AdYas.Add("Soner", 26);
+            //AdYas.Add("Caner", 32);
+            //AdYas.Add("Sakine", 54);
+            //AdYas.Add("Alpay", 57);
+
+            //foreach (var okunanDeger in AdYas)
+            //{
+            //    Console.WriteLine("Okunan değer: "+ okunanDeger);
+            //}
+
+
+            //Kendi Dictionary sınıfımı yazıyorum
+
+            MyDictionary<string, int> AdYas2 = new MyDictionary<string, int>();
+
+            Console.WriteLine(AdYas2.Count); 
+            AdYas2.Add("Soner", 26);
+
+            Console.WriteLine(AdYas2.Count);
+
+            for (int i = 0; i < AdYas2.Count; i++)
+            {
+                Console.WriteLine("[" + AdYas2.Keys[i] + "]" + "[" +  AdYas2.Values[i] + "]");
+            }
+        }
+    }
+}
